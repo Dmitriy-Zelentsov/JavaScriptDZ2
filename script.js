899144a4d4bdb75372548f2399ae506e16fec94e
Dmitriy-Zelentsov <zelentsov.dmitry@gmail.com>
2023-10-05 12:58:11 +0300

    ADD HW2

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // Задание 1 Создать переменные num1, num2 которые пользователь вводит с клавиатуры. Проверьте, что переменная num1 равна или меньше 1, а переменная num2 больше или равна 3.
 
-// var1 (if/else)
-let num1 = prompt("Введите число 1 равное или меньшее 1");
+// #1
+let numFirst = 0;
+let numSecond = 0;
+
+numFirst = parseInt(prompt('Введите первое число:', 0));
+console.log(numFirst);
+
+numSecond = parseInt(prompt('Введите второе число:', 0));
+console.log(numSecond);
+
+if ((numFirst <= 1) && (numSecond >= 3)) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// #2
+let test = true;
